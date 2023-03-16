83e1fb7ef7f328b9f10f194133257e1462c7f699
unixfy <alex@unixfy.net>
2023-03-15 18:33:50 -0700

    Implement basic homepage hero

---
+++ src/lib/Jumbotron.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Typed from "typed.js";
+
+    import {onMount, onDestroy} from "svelte";
+
+    let typedJsObject;
+    // on mount, init typed.js on an element with class 'typed'
+    onMount(() => {
+        typedJsObject = new Typed('.typed', {
+            // define the strings that typedjs will loop through
+            strings: ['transportation engineers.', 'transportation planners.', 'transportation enthusiasts.', 'transportation\'s future.'],
+            typeSpeed: 50,
+            backSpeed: 30,
+            backDelay: 1200,
+            loop: true
+        })
+    })
+
+    // don't forget to destroy typedJS object once the page is destroyed
+    onDestroy(() => {
+        if (typedJsObject) {
+            typedJsObject.destroy();
+        }
+    })
+</script>
+
+
+<div class="hero min-h-[50vh] bg-red-100">
+    <div class="hero-content flex-col text-center w-full">
+        <h1 class="text-4xl">USC Institute of Transportation Engineers</h1>
+        <h1 class="text-5xl font-bold">We are <span class="typed"></span></h1>
+        <div class="max-w-md">
+            <p class="pt-8">⬇ Scroll down to learn more</p>
+        </div>
+    </div>
+</div>
