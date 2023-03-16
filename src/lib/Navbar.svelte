3cc781340b895aa6351c28178766d3e8b878d4a1
unixfy <alex@unixfy.net>
2023-03-16 15:24:13 -0700

    Implement navbar generation from json

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,3 +1,27 @@
+<script>
+    let navbarItems = [
+        {
+            "name": "Home",
+            "url": "/"
+        },
+        {
+            "name": "About",
+            "url": "/about/"
+        },
+        {
+            "name": "Calendar",
+            "url": "/calendar/"
+        },
+        {
+            "name": "Opportunities",
+            "url": "/opportunities/"
+        },
+        {
+            "name": "Projects",
+            "url": "/projects/"
+        },
+    ]
+</script>
 <div class="bg-gray-100">
     <div class="ct navbar">
         <div class="navbar-start">
@@ -11,21 +35,11 @@
                 </label>
                 <ul tabindex="0"
                     class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Item 1</a></li>
-                    <li tabindex="0">
-                        <a class="justify-between">
-                            Parent
-                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                 viewBox="0 0 24 24">
-                                <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/>
-                            </svg>
-                        </a>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {#each navbarItems as navbarItem}
+                        <li>
+                            <a href="{navbarItem.url}">{navbarItem.name}</a>
+                        </li>
+                    {/each}
                 </ul>
             </div>
             <!--        long name on desktop, short name on mobile-->
@@ -37,21 +51,11 @@
         </div>
         <div class="navbar-end">
             <ul class="hidden lg:flex menu menu-horizontal px-1">
-                <li><a>Item 1</a></li>
-                <li tabindex="0">
-                    <a>
-                        Parent
-                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                             viewBox="0 0 24 24">
-                            <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/>
-                        </svg>
-                    </a>
-                    <ul class="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                </li>
-                <li><a>Item 3</a></li>
+                {#each navbarItems as navbarItem}
+                    <li>
+                        <a href="{navbarItem.url}">{navbarItem.name}</a>
+                    </li>
+                {/each}
             </ul>
             <a class="btn" href="/join/">Join Us</a>
         </div>
