8501f0efb9313986f956e04a09a4a2c03038bbaa
unixfy <alex@unixfy.net>
2023-03-16 15:32:55 -0700

    Revise page header display

---
+++ src/lib/PageHeader.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let title;
+    export let subtitle;
+</script>
+
+<div class="mb-8 py-8 border-b">
+    <h1 class="section-heading">{title}</h1>
+    <h2 class="section-subheading">{subtitle}</h2>
+</div>
