1e165b580c15d1051f5a5e78f3273c78186af1b3
unixfy <alex@unixfy.net>
2023-03-15 18:10:36 -0700

    Bootstrap basic footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,7 @@
+<div class="bg-gray-100 p-8">
+    <p>&copy; 2020-{new Date().getFullYear()} Institute of Transportation Engineers at the University of Southern
+        California.
+        Proudly powered by <a href="https://svelte.dev"
+                              target="_blank"
+                              rel="noopener">Svelte</a>.</p>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
-  import "../app.css";
-  import Navbar from "$lib/Navbar.svelte";
+    import "../app.css";
+    import Navbar from "$lib/Navbar.svelte";
+    import Footer from "$lib/Footer.svelte";
 </script>
 
-<Navbar />
+<Navbar/>
 
-<slot />
+<div class="min-h-screen">
+    <slot/>
+</div>
+
+<Footer/>
