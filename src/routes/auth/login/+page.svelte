82232dad06c034b769c15328faa04186bd9bbcfb
unixfy <alex@unixfy.net>
2023-03-17 15:02:41 -0700

    Build out basic login UI

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,1 +1,22 @@
-<h1>Login page (implement with firebase library)</h1>
+<div class="ct">
+    <div class="border rounded-md p-8 flex flex-col space-y-4 my-8 max-w-lg">
+        <h1 class="text-2xl">👋 Hello! Please sign in</h1>
+
+        <!--        Username input-->
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Username</span>
+            </label>
+            <input type="email" placeholder="admin@iteusc.com" class="input input-bordered w-full"/>
+        </div>
+        <!--        Pw input -->
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Password</span>
+            </label>
+            <input type="password" placeholder="Password" class="input input-bordered w-full"/>
+        </div>
+
+        <button class="btn btn-primary btn-lg">Submit</button>
+    </div>
+</div>
