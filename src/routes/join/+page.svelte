d76b00729a9df880a3f691cc33206c00b6ed128b
unixfy <alex@unixfy.net>
2023-03-16 22:37:50 -0700

    Scaffold join page

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -1,8 +1,19 @@
-<div class="bg-[url('/images/k-line.png')] bg-cover bg-center min-h-[50vh]">
-    <div class="bg-black bg-opacity-50 h-full w-full text-white flex flex-col">
-        <h1>Join USC ITE</h1>
-        <h2>Free polo + great experiences included.</h2>
+<div class="bg-[url('/images/k-line.png')] bg-cover bg-center h-[50vh]">
+    <div class="bg-black bg-opacity-50 h-full w-full text-white flex flex-col place-content-center text-center">
+        <h1 class="section-heading">Join USC ITE 👋</h1>
+        <h2 class="section-subheading">Free polo + great experiences included.</h2>
     </div>
 </div>
 
 <!--Show steps for joining using DaisyUI components-->
+<div class="ct text-center flex flex-col space-y-8 py-8">
+    <h2 class="section-heading">It's so easy to join</h2>
+    <ul class="steps w-full">
+        <li class="step step-primary">
+            <p>Join our mailing list</p>
+        </li>
+        <li class="step">We'll reach out with more info</li>
+        <li class="step">Success!</li>
+    </ul>
+    <iframe class="min-h-[100vh]" src="https://forms.gle/TQKCiMyscURNHgGMA" title="Google Form to subscribe to ITE newsletter"></iframe>
+</div>
