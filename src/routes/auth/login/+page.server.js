2d2f75f11ac40fb20f1d6c4f4ac1451508f86a3c
unixfy <alex@unixfy.net>
2023-03-17 15:56:00 -0700

    Bootstrap a functional (though DANGEROUS!) login form

--- src/routes/auth/login/+page.server.js
+++ src/routes/auth/login/+page.server.js
@@ -1,7 +1,23 @@
-import {redirect} from "@sveltejs/kit";
+import {fail, redirect} from "@sveltejs/kit";
+import {signInWithEmailAndPassword} from "firebase/auth";
 
 export async function load({locals}) {
     if (locals.user) {
         throw redirect(302, '/admin');
+    }
+}
+
+export const actions = {
+    default: async (event) => {
+        const data = await event.request.formData();
+
+        signInWithEmailAndPassword(event.locals.auth, data.get('username'), data.get('password')).then((userCredential) => {
+            console.log('success')
+            return {
+                success: true
+            }
+        }).catch((error) => {
+            return fail(400, error.message);
+        });
     }
 }
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,22 +1,25 @@
 <div class="ct">
-    <div class="border rounded-md p-8 flex flex-col space-y-4 my-8 max-w-lg">
+    <div class="border rounded-md p-8 my-8 max-w-lg">
         <h1 class="text-2xl">👋 Hello! Please sign in</h1>
 
-        <!--        Username input-->
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Username</span>
-            </label>
-            <input type="email" placeholder="admin@iteusc.com" class="input input-bordered w-full"/>
-        </div>
-        <!--        Pw input -->
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Password</span>
-            </label>
-            <input type="password" placeholder="Password" class="input input-bordered w-full"/>
-        </div>
+        <form class="flex flex-col space-y-4" method="POST">
+            <!--        Username input-->
+            <div class="form-control">
+                <label class="label" for="username">
+                    <span class="label-text">Username</span>
+                </label>
+                <input type="email" placeholder="admin@iteusc.com" class="input input-bordered w-full" id="username" name="username"/>
+            </div>
+            <!--        Pw input -->
+            <div class="form-control">
+                <label class="label" for="password">
+                    <span class="label-text">Password</span>
+                </label>
+                <input type="password" placeholder="Password" class="input input-bordered w-full" id="password" name="password"/>
+            </div>
 
-        <button class="btn btn-primary btn-lg">Submit</button>
+            <button class="btn btn-primary btn-lg">Submit</button>
+        </form>
+
     </div>
 </div>
