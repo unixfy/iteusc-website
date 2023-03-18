77b1e8530c0d7b00661f49e64b5705ab46973972
unixfy <alex@unixfy.net>
2023-03-17 17:10:10 -0700

    Bootstrap admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,7 +1,20 @@
 <script>
     import PageHeader from "$lib/PageHeader.svelte";
+
+    export let data;
 </script>
 
+<PageHeader title="Admin Area" subtitle="Hello there, {data.user.displayName || data.user.email}!"></PageHeader>
+
 <div class="ct">
-    <PageHeader title="Admin Area" subtitle="Manage posts and content on this site."></PageHeader>
+    <div class="section">
+        <h1 class="section-heading">Your details</h1>
+
+        <!--        Password change -->
+        <!--        Display name change -->
+
+<!--        Sign out button -->
+        <a href="/auth/logout" class="btn btn-primary">Sign out</a>
+
+    </div>
 </div>
