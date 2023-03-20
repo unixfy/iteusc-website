7830164b8095157e710f92221703f7ee8191e35c
unixfy <alex@unixfy.net>
2023-03-19 22:30:16 -0700

    Implement generator for <title> and meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,27 @@
     import "../app.css";
     import Navbar from "$lib/Navbar.svelte";
     import Footer from "$lib/Footer.svelte";
+    import {page} from "$app/stores";
+
+    let defaultTitle = "Welcome";
+    let defaultDescription = "The Institute of Transportation Engineers (ITE) at the University of Southern California is a chapter of the national ITE. We're devoted to hosting site tours, holding speaker sessions, sharing our passion for transportation, and changing the world of mobility - one Trojan at a time."
 </script>
 
+<svelte:head>
+    <!--    Interpolate page title if it's returned in each page's load() function, otherwise just show the value of defaultTitle -->
+    <title>{$page.data.title || defaultTitle} | USC ITE</title>
+    <!--    Interpolate page meta description if it's returned in the page's load() function, otherwise just return defaultMetaDescription. -->
+    <meta name="description"
+          content="{$page.data.description || defaultDescription}"/>
+</svelte:head>
+
+<!--Insert Navbar component-->
 <Navbar/>
 
+<!--Ensure the page container is at least 100vh high (that way the footer will always be below-the-fold)-->
 <div class="min-h-screen">
     <slot/>
 </div>
 
+<!--Insert footer component-->
 <Footer/>
