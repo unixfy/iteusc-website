235ad0cc169f267cd0c83913bbe537d0bb71f1e9
unixfy <alex@unixfy.net>
2023-05-14 23:43:36 -0400

    Add links on admin landing page to /admin/opportunities and /admin/projects

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -9,12 +9,18 @@
 
 <div class="ct">
     <div class="section">
+        <h1 class="section-heading">Admin interfaces</h1>
+        <!--        Button linking to /admin/opportunities, to manage opportunities -->
+        <a href="/admin/opportunities" class="btn btn-primary">Manage opportunities</a>
+        <!--        Button linking to /admin/projects, to manage projects -->
+        <a href="/admin/projects" class="btn btn-primary">Manage projects</a>
+
         <h1 class="section-heading">Your details</h1>
 
         <!--        Password change -->
         <!--        Display name change -->
 
-<!--        Sign out button -->
+        <!--        Sign out button -->
         <button class="btn btn-primary" on:click={() => signOut()}>Sign out</button>
 
     </div>
