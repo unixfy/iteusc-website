ea05c0a1e4bd06a0bcbd18bbda946a540570fa04
unixfy <alex@unixfy.net>
2023-05-17 17:23:36 -0400

    Remove outdated comment in admin/+page.server.js

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -1,6 +1,5 @@
 
 export async function load({locals}) {
-    // note that we can't just return locals.user because it's not serializable; we have to convert it to json first
     return {
         user: locals.user,
         title: "Admin Page",
