88a4d8023f996d0af844a31a07b0ca097322dc62
unixfy <alex@unixfy.net>
2023-05-18 14:09:48 -0400

    Implement CRUD for image grid items

---
+++ src/routes/admin/image-grid/+page.js
@@ -0,0 +1,34 @@
+import {firestore} from "$lib/firebase/client";
+import {collection, getDocs, orderBy, query} from "firebase/firestore";
+
+export async function load() {
+    // Create reference to 'image-grid' collection in Firestore
+    const ref = query(collection(firestore, 'image-grid'), orderBy('order', 'asc'));
+
+    // this array will be filled up with all our data by our Firebase query
+    let list = [];
+
+    // Query all projects from the collection
+    let documents = await getDocs(ref);
+
+    for (const doc of documents.docs) {
+        // firebase firestore is a bit stupid, so we need to iterate over every. single. child. and get details from there :/
+        // iterate over each opportunity, grab its data, and append it to list
+        const item = doc.data();
+
+        // Grab ID from document too
+        item.id = doc.id;
+
+        // must transform timestamp fields in our DB on our own, explicitly
+        item.createdAt = item.createdAt.toDate();
+        item.updatedAt = item.updatedAt.toDate();
+
+        // Force stringify everything with JSON before returning to prevent any non-POJOs that get through and cause 500s
+        list.push(JSON.parse(JSON.stringify(item)));
+    }
+
+    return {
+        items: list,
+        title: "Image Grid Admin"
+    }
+}
