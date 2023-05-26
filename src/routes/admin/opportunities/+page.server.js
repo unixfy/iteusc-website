c8815e99e7ead2d94e9e35aa20219fc3b2095d75
unixfy <alex@unixfy.net>
2023-05-26 15:05:24 -0400

    Use server side admin SDK to get data for admin pages (instaed of client side SDK)

--- src/routes/admin/opportunities/+page.server.js
+++ src/routes/admin/opportunities/+page.server.js
@@ -1,6 +1,39 @@
 import {firestore} from "$lib/firebase/server.server.js";
 import {fail} from "@sveltejs/kit";
 
+export async function load() {
+    // Create reference to 'opportunities' collection in Firestore
+    const ref = firestore.collection('opportunities');
+
+    // this array will be filled up with all our data by our Firebase query
+    let list = [];
+
+    // Query all opportunities from the collection
+    let documents = await ref.get();
+
+    for (const doc of documents.docs) {
+        // firebase firestore is a bit stupid, so we need to iterate over every. single. child. and get details from there :/
+        // iterate over each opportunity, grab its data, and append it to list
+        const item = doc.data();
+
+        // Grab ID from document too
+        item.id = doc.id;
+
+        // must transform timestamp fields in our DB on our own, explicitly
+        item.deadline = item.deadline.toDate();
+        item.createdAt = item.createdAt.toDate();
+        item.updatedAt = item.updatedAt.toDate();
+
+        // Force stringify everything with JSON before returning to prevent any non-POJOs that get through and cause 500s
+        list.push(JSON.parse(JSON.stringify(item)));
+    }
+
+    return {
+        opportunities: list,
+        title: "Opportunities Admin"
+    }
+}
+
 export const actions = {
     create: async ({request, locals}) => {
         const data = await request.formData();
