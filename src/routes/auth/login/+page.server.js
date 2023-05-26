9139e014795bc8aaf644151d37362303ce44fbca
unixfy <alex@unixfy.net>
2023-05-26 14:34:25 -0400

    Remove unnecessary imports

--- src/routes/auth/login/+page.server.js
+++ src/routes/auth/login/+page.server.js
@@ -1,6 +1,3 @@
-import {fail, redirect} from "@sveltejs/kit";
-import {signInWithEmailAndPassword} from "firebase/auth";
-
 export async function load() {
     return {
         // Do not allow search engines to index this page
