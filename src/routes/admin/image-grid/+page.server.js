29cf7e91ca0796c8de73b5011ae280a0f6b33930
unixfy <alex@unixfy.net>
2023-06-15 17:26:08 -0400

    Eliminate duplicated server-side get data from Firestore code

--- src/routes/admin/image-grid/+page.server.js
+++ src/routes/admin/image-grid/+page.server.js
@@ -1,31 +1,9 @@
 import {firestore, storage} from "$lib/firebase/server.server.js";
 import {fail} from "@sveltejs/kit";
+import getFirestoreDataServer from "$lib/firebase/getFirestoreDataServer.js";
 
 export async function load() {
-    // Create reference to 'image-grid'collection in Firestore
-    const ref = firestore.collection('image-grid').orderBy('order', 'asc');
-
-    // this array will be filled up with all our data by our Firebase query
-    let list = [];
-
-    // Query all projects from the collection
-    let documents = await ref.get();
-
-    for (const doc of documents.docs) {
-        // firebase firestore is a bit stupid, so we need to iterate over every. single. child. and get details from there :/
-        // iterate over each opportunity, grab its data, and append it to list
-        const item = doc.data();
-
-        // Grab ID from document too
-        item.id = doc.id;
-
-        // must transform timestamp fields in our DB on our own, explicitly
-        item.createdAt = item.createdAt.toDate();
-        item.updatedAt = item.updatedAt.toDate();
-
-        // Force stringify everything with JSON before returning to prevent any non-POJOs that get through and cause 500s
-        list.push(JSON.parse(JSON.stringify(item)));
-    }
+    const list = getFirestoreDataServer(firestore.collection('image-grid').orderBy('order', 'asc'));
 
     return {
         items: list,
