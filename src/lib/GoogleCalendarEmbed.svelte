c25c3efecc68ad5ac197f6200adb38a1bdbf94e1
unixfy <alex@unixfy.net>
2023-07-23 15:16:51 -0400

    Tweak aspect ratio for calendar embed on small devices

--- src/lib/GoogleCalendarEmbed.svelte
+++ src/lib/GoogleCalendarEmbed.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <!--iFrame embed for Google Calendar-->
-<iframe class="w-full min-h-[{minheight}] md:aspect-video my-8"
+<iframe class="w-full min-h-[{minheight}] aspect-square md:aspect-video my-8"
         src="https://calendar.google.com/calendar/embed?src=iteusc@gmail.com&ctz=America/Los_Angeles"
         title="Google Calendar embed showing ITE USC events"></iframe>
