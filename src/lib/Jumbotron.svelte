e0b2235ddfe36c8fba9c5a304dd6456e26fe3dbc
Alex <alex@unixfy.net>
2023-09-05 02:53:06 +0000

    Replace jumbotron stock image; closes #81

--- src/lib/Jumbotron.svelte
+++ src/lib/Jumbotron.svelte
@@ -25,8 +25,8 @@
 </script>
 
 
-<div class="hero min-h-[50vh] bg-[url('/images/s-bahn.jpg')] text-white bg-cover bg-center">
-    <div class="bg-black w-full h-full flex flex-col bg-opacity-40">
+<div class="hero min-h-[50vh] bg-[url('/images/jumbotron.jpg')] text-white bg-cover bg-center">
+    <div class="bg-black w-full h-full flex flex-col bg-opacity-50">
         <div class="hero-content m-auto flex-col text-center w-full">
             <h1 class="text-2xl md:text-3xl lg:text-4xl">USC Institute of Transportation Engineers</h1>
             <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold">We are <span class="typed"></span></h1>
