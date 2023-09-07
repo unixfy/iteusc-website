0aefa2adb738787949b976db1bfd4545f3ba4133
Alex <alex@unixfy.net>
2023-09-07 16:06:28 +0000

    Add Opengraph and Twitter meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,11 @@
     import "../app.css";
     import Navbar from "$lib/Navbar.svelte";
     import Footer from "$lib/Footer.svelte";
-    import {page} from "$app/stores";
+    import { page } from "$app/stores";
 
     let defaultTitle = "Welcome";
-    let defaultDescription = "The Institute of Transportation Engineers (ITE) at the University of Southern California is a chapter of the national ITE. We're devoted to hosting site tours, holding speaker sessions, sharing our passion for transportation, and changing the world of mobility - one Trojan at a time."
+    let defaultDescription =
+        "The Institute of Transportation Engineers (ITE) at the University of Southern California is a chapter of the national ITE. We're devoted to hosting site tours, holding speaker sessions, sharing our passion for transportation, and changing the world of mobility - one Trojan at a time.";
 </script>
 
 <svelte:head>
@@ -13,22 +14,50 @@
     <!--    This pattern of handling it ensures things will work even when JavaScript is not enabled in the client -->
     <title>{$page.data.title || defaultTitle} | USC ITE</title>
     <!--    Interpolate page meta description if it's returned in the page's load() function, otherwise just return defaultMetaDescription. -->
-    <meta name="description"
-          content="{$page.data.description || defaultDescription}"/>
+    <meta
+        name="description"
+        content={$page.data.description || defaultDescription}
+    />
+
+    <!-- OpenGraph meta tags -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://iteusc.com/" />
+    <meta
+        property="og:title"
+        content="{$page.data.title || defaultTitle} | USC ITE"
+    />
+    <meta
+        property="og:description"
+        content={$page.data.description || defaultDescription}
+    />
+    <meta property="og:image" content="/images/ite-portland.jpg" />
+
+    <!-- Twitter meta tags -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://iteusc.com/" />
+    <meta
+        property="twitter:title"
+        content="{$page.data.title || defaultTitle} | USC ITE"
+    />
+    <meta
+        property="twitter:description"
+        content={$page.data.description || defaultDescription}
+    />
+    <meta property="twitter:image" content="/images/ite-portland.jpg" />
 
     <!--    Send the meta tag that blocks robots from indexing, if we specify that this tag should be sent in page load() function -->
     {#if $page.data.noIndex}
-        <meta name="robots" content="noindex, nofollow"/>
+        <meta name="robots" content="noindex, nofollow" />
     {/if}
 </svelte:head>
 
 <!--Insert Navbar component-->
-<Navbar/>
+<Navbar />
 
 <!--Ensure the page container is at least 100vh high (that way the footer will always be below-the-fold)-->
 <div class="min-h-screen">
-    <slot/>
+    <slot />
 </div>
 
 <!--Insert footer component-->
-<Footer/>
+<Footer />
