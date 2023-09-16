a298d44d3d18f962d1726edb3c7c078bba3dc0e4
Alex <alex@unixfy.net>
2023-09-16 01:28:20 +0000

    Bootstrap basic hall of fame card component

---
+++ src/lib/HallofFameCard.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let image;
+    // Hall of Fame person's name
+    export let name;
+    // Hall of fame person's bio
+    export let bio;
+</script>
+
+<div class="flex flex-col sm:flex-row gap-8 align-items-center">
+    <img src={image} alt="Headshot for {name}" class="aspect-square h-48" />
+
+    <div class="bg-base-100 p-0">
+            <h2 class="text-3xl font-medium pb-4">{name}</h2>
+            <p>{bio}</p>
+    </div>
+</div>
--- src/routes/hall-of-fame/+page.svelte
+++ src/routes/hall-of-fame/+page.svelte
@@ -1,5 +1,22 @@
 <script>
-    import PageHeader from "$lib/PageHeader.svelte"
+    import PageHeader from "$lib/PageHeader.svelte";
+    import QueryEmptyAlert from "$lib/QueryEmptyAlert.svelte";
+    import HallofFameCard from "$lib/HallofFameCard.svelte";
+    import { getStorageDirectUrl } from "$lib/firebase/getStorageDirectUrl.js";
+
+    export let data;
 </script>
 
-<PageHeader title="Hall of Fame" subtitle="Meet our past USC ITE members."></PageHeader>
+<PageHeader title="Hall of Fame" subtitle="Meet our past USC ITE members." />
+
+<div class="ct pb-8">
+    {#each data.items as item}
+        <HallofFameCard
+            name={item.name}
+            bio={item.bio}
+            image={getStorageDirectUrl(item.image)}
+        />
+    {:else}
+        <QueryEmptyAlert />
+    {/each}
+</div>
