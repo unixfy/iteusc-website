e5217fd04529bcb5cdd03bbfacab1cdd9f778d5b
unixfy <alex@unixfy.net>
2023-10-01 17:32:31 -0700

    Remove redundant Firebase SDK imports in opportunities

--- src/routes/opportunities/+page.js
+++ src/routes/opportunities/+page.js
@@ -1,6 +1,3 @@
-import {firestore} from "$lib/firebase/client";
-import {collection, where, query} from "firebase/firestore";
-import getFirestoreData from "$lib/firebase/getFirestoreData.js";
 import {directus} from "$lib/directus/client.js";
 import {readItems} from "@directus/sdk";
 
