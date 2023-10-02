15d6cf7ec6e1a80ac9dd3731b3d7f9cb0e842ea5
unixfy <alex@unixfy.net>
2023-10-01 17:20:44 -0700

    Implement opportunities from Directus

--- src/routes/opportunities/+page.js
+++ src/routes/opportunities/+page.js
@@ -1,9 +1,20 @@
 import {firestore} from "$lib/firebase/client";
 import {collection, where, query} from "firebase/firestore";
 import getFirestoreData from "$lib/firebase/getFirestoreData.js";
+import {directus} from "$lib/directus/client.js";
+import {readItems} from "@directus/sdk";
 
 export async function load() {
-    const list = getFirestoreData(query(collection(firestore, 'opportunities'), where('published', '==', true)))
+    const list = directus.request(
+        readItems('opportunities', {
+                filter: {
+                    'status': {
+                        "_eq": "published"
+                    }
+                },
+                sort: ['deadline']
+            }
+        ))
 
     return {
         opportunities: list,
