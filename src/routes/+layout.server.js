aee9bf7dbcc026b0b5029516382ab241ae52da39
unixfy <alex@unixfy.net>
2023-10-02 21:02:13 -0700

    Remove Firebase SDK implementation

--- src/routes/+layout.server.js
+++
@@ -1,8 +0,0 @@
-import {getSession} from '$lib/firebase/getSession'
-
-export async function load({locals}) {
-    // locals.user has the user, but could be blank if not logged in
-    const {user} = locals
-    const session = getSession(user)
-    return {session}
-}
