4e1a7a88153eb01e796f0fe70c833bead654c334
unixfy <alex@unixfy.net>
2023-10-03 22:49:41 -0700

    Fix sort and add excerpt parsing for pages

--- src/routes/news/+page.js
+++ src/routes/news/+page.js
@@ -3,7 +3,7 @@ import {readItems} from "@directus/sdk";
 
 export async function load() {
     // grab all pages that are published and not hidden from this page
-    const list = directus.request(
+    const list = await directus.request(
         readItems('pages', {
                 filter: {
                     'status': {
@@ -13,12 +13,24 @@ export async function load() {
                         "_eq": false
                     }
                 },
-                sort: ['date_created'],
+                sort: ['-date_created'],
             }
         ))
 
+    // for each page we do some janky parsing to generate an excerpt from the "content" html
+    // by grabbing the first 20 words from the first p tag
+
+    let parsed_list = list.map((item) => {
+        let content = item.content;
+        // This grabs the first ~50 words from the content, strips HTML tags & nbsp, and adds ...
+        item.excerpt = content.split(" ").slice(0, 50).join(" ").replace(/<\/?[^>]+(>|$)/g, "").replace("&nbsp;", " ") + '...';
+        return item;
+    })
+
+    console.log(parsed_list)
+
     return {
-        items: list,
+        items: parsed_list,
         title: "News"
     }
 }
