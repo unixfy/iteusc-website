62d2f5fd456b55891602b743b21906446f3c6ec4
Alex <alex@unixfy.net>
2023-10-12 19:10:49 +0000

    Rearrange navbar items, closes #98

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,75 +1,90 @@
 <script>
     let navbarItems = [
         {
-            "name": "Home",
-            "url": "/"
+            name: "Home",
+            url: "/",
         },
         {
-            "name": "About",
-            "url": "/about/"
+            name: "About",
+            url: "/about/",
         },
         {
-            "name": "News",
-            "url": "/news/"
+            name: "News",
+            url: "/news/",
         },
         {
-            "name": "Photos",
-            "url": "/photos/"
+            name: "Photos",
+            url: "/photos/",
         },
         {
-            "name": "Hall of Fame",
-            "url": "/hall-of-fame/"
+            name: "Projects",
+            url: "/projects/",
         },
         {
-            "name": "Calendar",
-            "url": "/calendar/"
+            name: "Calendar",
+            url: "/calendar/",
         },
         {
-            "name": "Opportunities",
-            "url": "/opportunities/"
+            name: "Opportunities",
+            url: "/opportunities/",
         },
         {
-            "name": "Projects",
-            "url": "/projects/"
+            name: "Hall of Fame",
+            url: "/hall-of-fame/",
         },
-    ]
+    ];
 </script>
+
 <div class="bg-base-200">
     <div class="ct navbar">
         <div class="navbar-start w-auto">
             <!--            Mobile menu -->
             <div class="dropdown">
                 <label tabindex="0" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M4 6h16M4 12h8m-8 6h16"/>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h8m-8 6h16"
+                        />
                     </svg>
                 </label>
-                <ul tabindex="0"
-                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-10">
+                <ul
+                    tabindex="0"
+                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-10"
+                >
                     {#each navbarItems as navbarItem}
                         <li>
-                            <a href="{navbarItem.url}">{navbarItem.name}</a>
+                            <a href={navbarItem.url}>{navbarItem.name}</a>
                         </li>
                     {/each}
                 </ul>
             </div>
             <!--        display long name on desktop, short name on mobile-->
-            <a class="btn btn-ghost normal-case text-xl hidden xl:flex font-display" href="/">USC Institute of
-                Transportation
-                Engineers</a>
-            <a class="btn btn-ghost normal-case text-xl xl:hidden font-display" href="/">USC ITE</a>
+            <a
+                class="btn btn-ghost normal-case text-xl hidden xl:flex font-display"
+                href="/">USC Institute of Transportation Engineers</a
+            >
+            <a
+                class="btn btn-ghost normal-case text-xl xl:hidden font-display"
+                href="/">USC ITE</a
+            >
         </div>
         <!--        Area to put stuff that should be at the middle of the navbar-->
-        <div class="navbar-center hidden">
-        </div>
+        <div class="navbar-center hidden" />
         <!--        End of navbar (right side) - we put our navbar links here -->
         <div class="navbar-end w-full">
             <ul class="hidden lg:flex menu menu-horizontal px-1">
                 {#each navbarItems as navbarItem}
                     <li>
-                        <a href="{navbarItem.url}">{navbarItem.name}</a>
+                        <a href={navbarItem.url}>{navbarItem.name}</a>
                     </li>
                 {/each}
             </ul>
@@ -77,5 +92,4 @@
             <a class="btn btn-primary" href="/join/">Join Us</a>
         </div>
     </div>
-
 </div>
