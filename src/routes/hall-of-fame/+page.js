7b35daf3bc8ab0e25e4f61707798f68a3ad629bd
Alex <alex@unixfy.net>
2023-12-15 05:16:43 +0000

    Develop algorithm to group Hall of Fame items by induction year

--- src/routes/hall-of-fame/+page.js
+++ src/routes/hall-of-fame/+page.js
@@ -2,17 +2,39 @@ import { directus } from "$lib/directus/client.js";
 import { readItems } from "@directus/sdk";
 
 export async function load() {
-    const hall_of_fame_items = directus.request(
+    // Pull hall of fame items from Directus
+    const hall_of_fame_items_req = directus.request(
         readItems('hall_of_fame', {
             filter: {
                 'status': {
                     "_eq": "published"
                 }
             },
-            sort: ['year_inducted', 'last_name', 'first_name'],
+            // note that we sort by year_inducted first, such that most recently inducted people show up first
+            sort: ['-year_inducted', 'last_name', 'first_name'],
             fields: ['*,degrees.degrees_id.name']
         }
         ))
+
+    // Begin processing the items
+    let hall_of_fame_items_raw = await hall_of_fame_items_req
+
+    // Init a Map to store the hall of fame items after processing (note that we use Map to preserve order)
+    const hall_of_fame_items = new Map([])
+
+    // Loop over each hall of fame item from the Directus response
+    hall_of_fame_items_raw.forEach(item => {
+        // Grab the induction year for the item
+        const year = item.year_inducted;
+
+        // Check if the induction year is already in the Map, if not, add it
+        if (!hall_of_fame_items.get(year)) {
+            hall_of_fame_items.set(year, [])
+        }
+
+        // Add the item to the Map item with the same year as the item in question
+        hall_of_fame_items.get(year).push(item);
+    });
 
     return {
         items: hall_of_fame_items,
