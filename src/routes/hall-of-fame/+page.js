6d87478a24f16d5c5252904b3bcd0b4c560a5286
Alex <alex@unixfy.net>
2023-12-15 02:13:38 +0000

    Use clearer var name in hall of fame fetch logic

--- src/routes/hall-of-fame/+page.js
+++ src/routes/hall-of-fame/+page.js
@@ -2,7 +2,7 @@ import { directus } from "$lib/directus/client.js";
 import { readItems } from "@directus/sdk";
 
 export async function load() {
-    const list = directus.request(
+    const hall_of_fame_items = directus.request(
         readItems('hall_of_fame', {
             filter: {
                 'status': {
@@ -15,7 +15,7 @@ export async function load() {
         ))
 
     return {
-        items: list,
+        items: hall_of_fame_items,
         title: "Hall of Fame",
         description: "This Hall of Fame honors those who led USC ITE during their college career and have continued to make an impact in their profession, or in other ways brought distinction to their alma mater."
     }
