05888dc9a23ea21350ee8a3cdef1028c0f65e64d
Alex <alex@unixfy.net>
2023-12-28 19:33:49 +0000

    Primitively build logic for grouping hall of fame items by year

--- src/routes/photos/+page.js
+++ src/routes/photos/+page.js
@@ -2,7 +2,7 @@ import { directus } from "$lib/directus/client.js"
 import { readItems } from "@directus/sdk"
 
 export async function load() {
-    const list = directus.request(
+    const photos_req = directus.request(
         readItems('photo_archive_items', {
             filter: {
                 'status': {
@@ -12,11 +12,31 @@ export async function load() {
             sort: ["-date"],
             fields: ['*,image.*']
         }
-    ))
+        ))
+
+    let photos_raw = await photos_req
+
+    // use date-fns to parse the year from each date, then add a "year" field to each photo item
+    photos_raw = photos_raw.map((photo) => {
+        const date = new Date(photo.date)
+        photo.year = date.getFullYear()
+        return photo
+    })
+
+    let photos_list = new Map([])
+
+    // group photos by year
+    photos_raw.forEach(photo => {
+        const year = photo.year
+        if (!photos_list.get(year)) {
+            photos_list.set(year, [])
+        }
+        photos_list.get(year).push(photo)
+    })
 
     return {
         streamed: {
-            items: list
+            items: photos_list
         },
         title: "Photos"
     }
