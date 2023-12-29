2bba67eae45d2f756602a66e4af478364f1e4b09
Alex <alex@unixfy.net>
2023-12-29 03:47:48 +0000

    Refactor photos page to include divisions by event year

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -21,33 +21,43 @@
         Come join us and become part of the transportation family at USC.
     </p>
 
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {#await data.streamed.items}
+    {#await data.streamed.items}
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
             {#each new Array(16) as item}
                 <div class="w-full h-64 rounded-lg bg-base-300 animate-pulse" />
             {/each}
-        {:then items}
-            {#each items as item}
-                <a href={item.url} target="_blank" rel="noreferrer">
-                    <div
-                        class="bg-cover bg-center flex rounded-lg aspect-square p-4 md:hover:scale-105 transition-all"
-                        style="background-image: url('{getStorageDirectUrl(
-                            item.image.id
-                        )}?format=webp&width={Math.ceil(
-                            screenSize / 1.5
-                        )}&quality=50');"
-                    >
-                        <div
-                            class="m-auto p-4 bg-black bg-opacity-50 rounded-md text-center text-white"
-                        >
-                            <p class="font-bold text-lg">{item.name}</p>
-                            <p class="text-sm">
-                                {format(parseISO(item.date), "LLLL d, y")}
-                            </p>
-                        </div>
-                    </div>
-                </a>
-            {/each}
-        {/await}
-    </div>
+        </div>
+    {:then items}
+        {#each items as [year, items]}
+            <div class="my-8">
+                <h2 class="section-heading">{year}</h2>
+                <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    {#each items as item}
+                        <a href={item.url} target="_blank" rel="noreferrer">
+                            <div
+                                class="bg-cover bg-center flex rounded-lg aspect-square p-4 md:hover:scale-105 transition-all"
+                                style="background-image: url('{getStorageDirectUrl(
+                                    item.image.id,
+                                )}?format=webp&width={Math.ceil(
+                                    screenSize / 1.5,
+                                )}&quality=50');"
+                            >
+                                <div
+                                    class="m-auto p-4 bg-black bg-opacity-50 rounded-md text-center text-white"
+                                >
+                                    <p class="font-bold text-lg">{item.name}</p>
+                                    <p class="text-sm">
+                                        {format(
+                                            parseISO(item.date),
+                                            "LLLL d, y",
+                                        )}
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    {/each}
+                </div>
+            </div>
+        {/each}
+    {/await}
 </div>
