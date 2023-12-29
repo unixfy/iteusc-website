0378272ff2d95294b6ab853d567cdb11f2227856
Alex <alex@unixfy.net>
2023-12-29 04:14:22 +0000

    Refactor photo loading and grouping logic

--- src/routes/photos/+page.js
+++ src/routes/photos/+page.js
@@ -1,8 +1,10 @@
 import { directus } from "$lib/directus/client.js"
 import { readItems } from "@directus/sdk"
+import { groupDataByProperty } from "$lib/directus/groupDataByProperty"
 
 export async function load() {
-    const photos_req = directus.request(
+    // request photo archive items from directus, then parse the year from each date and group by year (all in one promise)
+    const photos_list = directus.request(
         readItems('photo_archive_items', {
             filter: {
                 'status': {
@@ -12,27 +14,17 @@ export async function load() {
             sort: ["-date"],
             fields: ['*,image.*']
         }
-        ))
-
-    let photos_raw = await photos_req
-
-    // use date-fns to parse the year from each date, then add a "year" field to each photo item
-    photos_raw = photos_raw.map((photo) => {
-        const date = new Date(photo.date)
-        photo.year = date.getFullYear()
-        return photo
-    })
-
-    let photos_list = new Map([])
-
-    // group photos by year
-    photos_raw.forEach(photo => {
-        const year = photo.year
-        if (!photos_list.get(year)) {
-            photos_list.set(year, [])
-        }
-        photos_list.get(year).push(photo)
-    })
+        )).then((photos) => {
+            // use date-fns to parse the year from each date, then add a "year" field to each photo item
+            photos = photos.map((photo) => {
+                const date = new Date(photo.date)
+                photo.year = date.getFullYear()
+                return photo
+            })
+            // group the photos by year using the groupDataByProperty function
+            const photos_grouped = groupDataByProperty(photos, 'year')
+            return photos_grouped
+        })
 
     return {
         streamed: {
