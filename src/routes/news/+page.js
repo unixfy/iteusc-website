61e9813d47075df27a42932c75407f328819d798
Alex <alex@unixfy.net>
2024-01-08 21:37:56 +0000

    Refactor /news page.js to use .then for processing

--- src/routes/news/+page.js
+++ src/routes/news/+page.js
@@ -1,36 +1,36 @@
-import {directus} from "$lib/directus/client.js";
-import {readItems} from "@directus/sdk";
-import {decodeHTML} from "entities";
+import { directus } from "$lib/directus/client.js";
+import { readItems } from "@directus/sdk";
+import { decodeHTML } from "entities";
 
 export async function load() {
     // grab all pages that are published and not hidden from this page
-    const list = await directus.request(
+    // then for each page we do some janky parsing to generate an excerpt from the "content" html
+    // by grabbing the first 50 words from the first p tag
+    const list = directus.request(
         readItems('pages', {
-                filter: {
-                    'status': {
-                        "_eq": "published"
-                    },
-                    'hidden_from_news': {
-                        "_eq": false
-                    }
+            filter: {
+                'status': {
+                    "_eq": "published"
                 },
-                sort: ['-date_posted', 'title'],
-                fields: ['*','author.first_name', 'author.last_name']
-            }
-        ))
-
-    // for each page we do some janky parsing to generate an excerpt from the "content" html
-    // by grabbing the first 20 words from the first p tag
-
-    let parsed_list = list.map((item) => {
-        let content = item.content;
-        // This grabs the first ~50 words from the content, strips HTML tags & nbsp, and adds ...
-        item.excerpt = decodeHTML(content.split(" ").slice(0, 50).join(" ").replace(/<\/?[^>]+(>|$)/g, "")) + '...';
-        return item;
-    })
+                'hidden_from_news': {
+                    "_eq": false
+                }
+            },
+            sort: ['-date_posted', 'title'],
+            fields: ['*', 'author.first_name', 'author.last_name']
+        }
+        )).then((items) => {
+            items.map((item) => {
+                let content = item.content;
+                // This grabs the first ~50 words from the content, strips HTML tags & nbsp, and adds ...
+                item.excerpt = decodeHTML(content.split(" ").slice(0, 50).join(" ").replace(/<\/?[^>]+(>|$)/g, "")) + '...';
+                return item;
+            })
+            return items
+        })
 
     return {
-        items: await parsed_list,
+        items: await list,
         title: "News",
         socialImage: "/images/portland-train.jpg"
     }
