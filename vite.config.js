eca7bd928b2a8d24f522329c4e2c15a03fd2e4a1
Alex <alex@unixfy.net>
2024-01-08 20:36:50 +0000

    Add Sentry integration for error tracking

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "unixfy",
+            project: "iteusc-website",
+            url: "https://glitchtip.unixfy.net/"
+        }
+    }), sveltekit()]
 });
