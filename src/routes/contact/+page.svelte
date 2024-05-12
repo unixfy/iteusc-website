656c95839ba858cc8821520b96335a0fcc038e80
Alex <alex@unixfy.net>
2024-05-12 23:44:06 +0000

    Add a very basic contact us page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import PageHeader from "$lib/PageHeader.svelte";
+</script>
+
+<PageHeader title="Contact Us" subtitle="Learn more about or work with our ITE chapter."
+></PageHeader>
+
+<div class="ct">
+    <p>We can be reached via email: iteusc [at] gmail.com.</p>
+</div>
