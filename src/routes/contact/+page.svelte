a00c8aec05a6ad11d026a443a89c30127405c79a
Alex <alex@unixfy.net>
2024-05-13 01:05:51 +0000

    Embed mailchimp contact form on /contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,10 +1,20 @@
 <script>
     import PageHeader from "$lib/PageHeader.svelte";
+
+    let name = "";
+    let email = "";
+    let message = "";
 </script>
 
-<PageHeader title="Contact Us" subtitle="Learn more about or work with our ITE chapter."
+<PageHeader
+    title="Contact Us"
+    subtitle="Learn more about or work with our ITE chapter."
 ></PageHeader>
 
 <div class="ct">
-    <p>We can be reached via email: <b>iteusc [at] gmail.com</b>.</p>
+    <iframe
+        class="w-full min-h-[500px] aspect-square md:aspect-video my-8"
+        title="USC ITE Mailchimp contact form"
+        src="https://us17.list-manage.com/contact-form?u=0b0f9abcfdce6d20a98515274&form_id=d7d8b612580a749bf7e39b257458da16"
+    ></iframe>
 </div>
