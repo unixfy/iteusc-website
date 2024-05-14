c73b36d47ad1288becec3888183ce09430569c40
Alex <alex@unixfy.net>
2024-05-14 21:37:55 +0000

    Implement video block component

---
+++ src/lib/blocks/Video.svelte
@@ -0,0 +1,8 @@
+<script>
+    // url for video
+    export let video;
+</script>
+
+<video src={video} controls width="100%" height="100%" class="my-8 rounded-lg drop-shadow-xl">
+    <p>Your browser doesn't seem to support this video.</p>
+</video>
