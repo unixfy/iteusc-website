a73b969908eefbd3730eff8bcb7123a2e5c69e81
Alex <alex@unixfy.net>
2024-05-14 22:18:33 +0000

    Improve skeleton on /photos

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -23,6 +23,7 @@
     </p>
 
     {#await data.streamed.items}
+        <div class="section-heading-skeleton"></div>
         <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {#each new Array(16) as item}
                 <div class="w-full h-64 rounded-lg skeleton" />
