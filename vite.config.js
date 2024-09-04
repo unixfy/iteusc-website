a6f3b74559b42de3c07912603b0ffab2010ccacd
Alex <alex@unixfy.net>
2024-09-04 18:35:33 +0000

    Remove Sentry (no support for CF Pages)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "unixfy",
-            project: "iteusc-website",
-            url: "https://glitchtip.unixfy.net/"
-        }
-    }), sveltekit()]
+	plugins: [sveltekit()]
 });
