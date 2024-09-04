543bb55965b8cfc3780009f6a28f8f9e48af96c5
Alex <alex@unixfy.net>
2024-09-04 18:32:20 +0000

    Migrate to Cloudflare Pages (closes #169)

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-cloudflare';
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			edge: false
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
