9e3cd901253d40ccbccb89afe1f917d0220d3fc5
Alex <alex@unixfy.net>
2024-09-29 20:54:57 +0000

    Fix grid CSS on projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,27 +1,28 @@
 <script>
     import PageHeader from "$lib/PageHeader.svelte";
     import QueryEmptyAlert from "$lib/QueryEmptyAlert.svelte";
-    import {getStorageDirectUrl} from "$lib/directus/getStorageDirectUrl.js";
+    import { getStorageDirectUrl } from "$lib/directus/getStorageDirectUrl.js";
 
     export let data;
 </script>
 
 <PageHeader
-        title="Our Projects"
-        subtitle="How we're impacting the world of transportation."
+    title="Our Projects"
+    subtitle="How we're impacting the world of transportation."
 />
 <div class="ct">
-    {#each data.projects as project}
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 mb-8">
-
-            <label for="project-modal">
+    <div class="grid sm:grid-cols-2 md:grid-cols-3 mb-8">
+        {#each data.projects as project}
+            <label for="project-modal" class="flex">
                 <div
-                        class="card card-compact bg-base-200 border border-base-200 hover:shadow-xl my-4 mx-8 hover:scale-[1.05] transition-all hover:bg-base-300 hover:cursor-pointer"
+                    class="card card-compact bg-base-200 border border-base-200 hover:shadow-xl my-4 mx-8 hover:scale-[1.05] transition-all hover:bg-base-300 hover:cursor-pointer"
                 >
-                    <figure>
+                    <figure class="aspect-video">
                         <img
-                                src="{getStorageDirectUrl(project.image)}?quality=50&width=400&format=webp"
-                                alt={project.name}
+                            src="{getStorageDirectUrl(
+                                project.image,
+                            )}?quality=50&width=400&format=webp"
+                            alt={project.name}
                         />
                     </figure>
                     <div class="card-body">
@@ -34,13 +35,13 @@
                 </div>
             </label>
 
-            <input type="checkbox" id="project-modal" class="modal-toggle"/>
+            <input type="checkbox" id="project-modal" class="modal-toggle" />
             <label for="project-modal" class="modal cursor-pointer">
                 <div class="modal-box relative">
                     <label
-                            for="project-modal"
-                            class="btn btn-sm btn-circle absolute right-2 top-2"
-                    >✕</label
+                        for="project-modal"
+                        class="btn btn-sm btn-circle absolute right-2 top-2"
+                        >✕</label
                     >
                     <h3 class="text-lg font-bold">{project.name}</h3>
                     <p class="py-4">
@@ -48,8 +49,8 @@
                     </p>
                 </div>
             </label>
-        </div>
-    {:else}
-        <QueryEmptyAlert/>
-    {/each}
+        {:else}
+            <QueryEmptyAlert />
+        {/each}
+    </div>
 </div>
