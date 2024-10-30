1b2b46dee739f8c14d41f8dcfaf38d1234943d6b
Alex Wang <unixfy@users.noreply.github.com>
2024-10-30 12:49:16 -0700

    Update calendar page embed url

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -6,7 +6,7 @@
 <PageHeader title="Events Calendar" subtitle="See what's going on at USC ITE."></PageHeader>
 
 <div class="ct">
-    <a href="https://calendar.google.com/calendar/u/0/r?cid=iteusc%40gmail.com" class="btn btn-primary btn-block btn-lg" target="_blank"
+    <a href="https://calendar.google.com/calendar/embed?src=qpkajdr9vj8ov3dnr9u7p00i2cicnke7%40import.calendar.google.com&ctz=America%2FLos_Angeles" class="btn btn-primary btn-block btn-lg" target="_blank"
        rel="noreferrer">Subscribe to our Calendar</a>
 
     <GoogleCalendarEmbed minheight="500px"/>
