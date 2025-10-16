df0d248d1425302846f4808cfaa42a8733fff1ab
Alex Wang <unixfy@users.noreply.github.com>
2025-10-15 22:26:22 -0400

    Update calendar subscription link

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -6,7 +6,7 @@
 <PageHeader title="Events Calendar" subtitle="See what's going on at USC ITE."></PageHeader>
 
 <div class="ct">
-    <a href="https://calendar.google.com/calendar/embed?src=qpkajdr9vj8ov3dnr9u7p00i2cicnke7%40import.calendar.google.com&ctz=America%2FLos_Angeles" class="btn btn-primary btn-block btn-lg" target="_blank"
+    <a href="https://calendar.google.com/calendar/embed?src=uo93k1ra6d1030ufvo10mhm5bkra305g%40import.calendar.google.com&ctz=America%2FLos_Angeles" class="btn btn-primary btn-block btn-lg" target="_blank"
        rel="noreferrer">Subscribe to our Calendar</a>
 
     <GoogleCalendarEmbed minheight="500px"/>
