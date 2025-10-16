efb2d349af3ba88be06b732533f30573f8a51fb2
Alex Wang <unixfy@users.noreply.github.com>
2025-10-15 22:26:37 -0400

    Update Google Calendar embed source URL

--- src/lib/GoogleCalendarEmbed.svelte
+++ src/lib/GoogleCalendarEmbed.svelte
@@ -5,5 +5,5 @@
 
 <!--iFrame embed for Google Calendar-->
 <iframe class="w-full min-h-[{minheight}] aspect-square md:aspect-video my-8"
-        src="https://calendar.google.com/calendar/embed?src=qpkajdr9vj8ov3dnr9u7p00i2cicnke7%40import.calendar.google.com&ctz=America%2FLos_Angeles"
+        src="https://calendar.google.com/calendar/embed?src=uo93k1ra6d1030ufvo10mhm5bkra305g%40import.calendar.google.com&ctz=America%2FLos_Angeles"
         title="Google Calendar embed showing ITE USC events"></iframe>
